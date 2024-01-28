efbbc08174cec034ffb2f76cd7a6a442f30a81f4
darksh0w <dark.archangel.michael@gmail.com>
2024-01-28 14:54:39 +0800

    allow to add builtin function

---
+++ extBuiltinFunc.go
@@ -0,0 +1,27 @@
+package tengo
+
+func (s *Script) AddFunction(name string, val func(args ...Object) (Object, error)) {
+	if s.builtinIsExist(name) {
+		return
+	}
+	builtinFuncs = append(builtinFuncs, &BuiltinFunction{Name: name, Value: val})
+}
+
+func (s *Script) AddFuncs(funcs []*BuiltinFunction) {
+	for _, f := range funcs {
+		if s.builtinIsExist(f.Name) {
+			return
+		}
+		builtinFuncs = append(builtinFuncs, f)
+	}
+}
+
+func (s *Script) builtinIsExist(name string) (exist bool) {
+	for _, bf := range builtinFuncs {
+		if bf.Name == name {
+			exist = true
+		}
+	}
+
+	return
+}
